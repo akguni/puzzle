64c765af31ecc1959834ba193a51b52c4676825e
akguni <akguni@yahoo.com>
2021-09-07 11:15:24 +0200

    working ok needs recursion

--- puzzle.py
+++ puzzle.py
@@ -7,48 +7,41 @@ solved = False
 while solved == False:
     cluestr = input('Give me clues as x,y coordinates and shape number, e.g. 2,1,0 for a 0 shape in third cell right and second up from bottom left: ')
     clue = [int (n) for n in cluestr.split(',')]
-    matrix[clue[0]][clue[1]]=clue[2]
+    matrix[clue[0]][clue[1]] = clue[2]
  # scan the x axis using the same y coordinate as given clue
     for x in range(matrix_size):
         # process only blank cells, skip otherwise
         if matrix[x][clue[1]] == None:
             # list all possibilities first
             possibilities = [i for i in range(matrix_size)]
-            # scan everything else in the same row and eliminate possibilities
+            # scan everything else in the same row/column and eliminate possibilities
             for xi in range(matrix_size):
-                # scan everything else in the same column and eliminate possibilities
-                for yi in range(matrix_size):
-                    # only filled cells are relevant
-                    if matrix[xi][yi] != None:
-                        possibilities.remove(matrix[xi][yi])
-                            
-                    if len(possibilities) == 1:
-                        matrix[x][clue[1]] = possibilities[0]
-                        if x == solution[0] and clue[1] == solution[1]:
-                            break
-
-
-# Now do the same for y axis
- # scan the y axis using the same x coordinate as given clue
+                if matrix[xi][clue[1]] in possibilities:
+                    possibilities.remove(matrix[xi][clue[1]])
+            for yi in range(matrix_size):
+                if matrix[x][yi] in possibilities:
+                    possibilities.remove(matrix[x][yi])
+            if len(possibilities) == 1:
+                matrix[x][clue[1]] = possibilities[0]
+                if x == solution[0] and clue[1] == solution[1]:
+                    break
+    # Now do the same for y axis
+    # scan the y axis using the same x coordinate as given clue
     for y in range(matrix_size):
-        # process only blank cells, skip otherwise
+    # process only blank cells, skip otherwise
         if matrix[clue[0]][y] == None:
             # list all possibilities first
             possibilities = [i for i in range(matrix_size)]
-            # scan everything else in the same column and eliminate possibilities
+            # scan everything else in the same row/column and eliminate possibilities
+            for xi in range(matrix_size):
+                if matrix[xi][y] in possibilities:
+                    possibilities.remove(matrix[xi][y])
             for yi in range(matrix_size):
-                # scan everything else in the same row and eliminate possibilities
-                for xi in range(matrix_size):
-                    # only filled cells are relevant
-                    if matrix[xi][yi] != None:
-                        possibilities.remove(matrix[xi][yi])
-
-                            
-                    if len(possibilities) == 1:
-                        matrix[clue[0]][y] = possibilities[0]
-                        if y == solution[1] and clue[0] == solution[0]:
-                            break
-
-
+                if matrix[clue[0]][yi] in possibilities:
+                    possibilities.remove(matrix[clue[0]][yi])
+            if len(possibilities) == 1:
+                matrix[clue[0]][y] = possibilities[0]
+                if x == solution[0] and clue[1] == solution[1]:
+                    break
 
 print('solution is ' + possibilities[0])
