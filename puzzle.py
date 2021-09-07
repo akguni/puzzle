7bf90f19f55b1389411ee7417ca59860906173be
akguni <akguni@yahoo.com>
2021-09-07 15:17:59 +0200

    solving all except solution cell

--- puzzle.py
+++ puzzle.py
@@ -14,8 +14,8 @@ def new_input(clue, matrix):
                 if matrix[x][yi] in possibilities:
                     possibilities.remove(matrix[x][yi])
             if len(possibilities) == 1:
-                matrix[x][clue[1]] = possibilities[0]
-                new_input([x,clue[1],matrix[x][clue[1]]], matrix)
+                if x == solution[0] and clue[1] == solution[1]: return possibilities[0]
+                new_input([x,clue[1],possibilities[0]], matrix)
 
     for y in range(matrix_size):
     # process only blank cells, skip otherwise
@@ -30,8 +30,8 @@ def new_input(clue, matrix):
                 if matrix[clue[0]][yi] in possibilities:
                     possibilities.remove(matrix[clue[0]][yi])
             if len(possibilities) == 1:
-                matrix[clue[0]][y] = possibilities[0]
-                new_input([clue[0],y,matrix[clue[0]][y]], matrix)
+                if clue[0] == solution[0] and y == solution[1]: return possibilities[0]
+                new_input([clue[0],y,possibilities[0]], matrix)
 
 
 matrix_size = int(input('Enter 4 or 5 for Matrix Size: '))
@@ -39,13 +39,11 @@ matrix = [[None for i in range(matrix_size)] for j in range(matrix_size)]
 solutionstr = input('Enter x,y coordinates of the cell to be solved, e.g. 0,0 for bottom left corner: ')
 # apparently using subscriptable list is clunky as there is a more elegant way using maps
 solution = [int(n) for n in solutionstr.split(',')]
-solved = False
-while solved == False:
+while True:
     cluestr = input('Give me clues as x,y coordinates and shape number, e.g. 2,1,0 for a 0 shape in third cell right and second up from bottom left: ')
     clue = [int(n) for n in cluestr.split(',')]
-    matrix[clue[0]][clue[1]] = clue[2]
-    new_input(clue, matrix)
-    if matrix[solution[0]][solution[1]]: solved = True
-
-print('solution is ' + possibilities[0])
-
+    
+    solved = new_input(clue, matrix)
+    if solved != None:
+        print(solved)
+        break
